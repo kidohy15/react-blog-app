8eb85ca3ab4c6adb09254da01a9b63f04773466a
kidohy15 <kidohy307@gmail.com>
2023-12-11 20:21:25 +0900

    다크 모드 테마 context 생성, 테마모드 버튼 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,30 @@
+import ThemeContext from "context/ThemeContext";
+import { BsSun, BsMoonFill } from "react-icons/bs";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const context = useContext(ThemeContext);
+  console.log(context);
+  
   return (
     <footer>
       <Link to="/posts/new">글쓰기</Link>
       <Link to="/posts">게시글</Link>
       <Link to="/profile">프로필</Link>
+      <>
+        {context.theme === "light" ? (
+          <BsSun
+            onClick={context.toggleMode}
+            className="footer__theme-btn"
+          />
+        ) : (
+          <BsMoonFill
+            onClick={context.toggleMode}
+            className="footer__theme-btn"
+          />
+        )}
+      </>
     </footer>
   );
 }
